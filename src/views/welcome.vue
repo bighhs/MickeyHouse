2bfde8f6c0dabedaafefdc9662b99a0c5f04ecb9
chen_junyang <chen_junyang@icekredit.com>
2022-04-18 17:20:51 +0800

    init

---
+++ src/views/welcome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main_container">
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'welcome',
+    data() {
+        return{
+
+        }
+    },
+}
+</script>
+<style scoped>
+
+</style>
